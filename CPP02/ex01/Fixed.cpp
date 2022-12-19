ec9eafa1780bc0ffa3a480cc4145e7b60b565ba3
gohar <goharsimonyan5@gmail.com>
2022-12-19 20:34:18 +0400

    add fixed point

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -1,6 +1,9 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : fixed(0) { std::cout << "Default constructor called!\n"; }
+Fixed::Fixed(const int inp) : fixed(0)
+{
+	std::cout << "Default constructor called!\n";
+}
 
 Fixed::Fixed(const Fixed &other)
 {
@@ -13,6 +16,16 @@ Fixed& Fixed::operator=(const Fixed &other)
 	std::cout << "Copy assignment operator called!\n";
 	fixed = other.getRawBits();
 	return *this;
+}
+
+int Fixed::float_to_fixed(float inp)
+{
+	return static_cast<int>(inp * (1 << bits));
+}
+
+float Fixed::fixed_to_float(int inp)
+{
+    return (static_cast<float>(inp) / (1 << bits));
 }
 
 Fixed::~Fixed() { std::cout << "Destructor called!\n"; }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -7,10 +7,12 @@ private:
 	int					fixed;
 	static const int	bits = 0;
 public:
-	Fixed();
+	Fixed(const int inp);
 	Fixed(const Fixed &other);
 	~Fixed();
 	Fixed&	operator=(const Fixed& other);
+	int		float_to_fixed(float inp);
+	float	fixed_to_float(int inp);
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
 };
